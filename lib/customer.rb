388c77f9a154742545963a28c0794448d99796ac
Giorgia <giorgia1london@gmail.com>
2014-11-08 23:47:39 +0000

    customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,30 @@
 class Customer
 
 	attr_reader :name
-	attr_accessor :phone_no
+	attr_accessor :phone_no, :order
 
 	def initialize(name, number)
 		@name = name
-		@phone_no = number
+		@phone_no = number.to_s
+		@order = []
 	end
+
+	def change_no(new_number)
+		@phone_no = new_number.to_s
+	end
+
+	def choose_dish(menu, dish)
+		menu.each{ |plate| order << plate if plate == dish }
+	end
+
+	def total_due
+
+	end
+
+	def paid?
+
+	end
+	
+
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,7 +3,9 @@ require 'customer'
 describe Customer do
 
 	let(:customer) { Customer.new(:name, :num) }
-
+	let(:menu) { double :menu }
+	let(:dish) { double :dish }
+	
 it 'has a name' do
 	expect(customer.name).not_to be nil
 end
@@ -13,17 +15,23 @@ it 'has a phone number' do
 end
 
 it 'can cange the phone number in case provided the wrong one' do
-
+	expect(customer.phone_no).not_to be nil
+	customer.change_no(:new_num)
+	expect(customer.phone_no).not_to be nil
 end
 
-it 'can select dishes from the menu'
+it 'can select dishes from the menu' do
+	customer.choose_dish(menu,dish)
+	expect(customer.order).to be([dish])
+end
 
-it 'has a order that is a list of dishes with price'
+it 'has an order that is a list of dishes with price'
 
 it 'can ask for the total bill'
 
 it 'has to pay a total'
 
+it 'receives order placed confirmation texts'
 
 
 
