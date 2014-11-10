7cc306ec58aeb3415a9fb384611c538af14ad51f
Giorgia <giorgia1london@gmail.com>
2014-11-10 17:21:41 +0000

    still wrking on message

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,20 +1,19 @@
-# require 'message'
+require 'message'
 
-# describe Message do
-# 	let(:sms) { Message.new }
-# 	let(:customer_number) {double :number}
+describe Message do
+	let(:sms) { Message.new }
+	let(:message){ double :message }
 
 
-# it 'can send a text if a phone number is provided' do 
-# 	message = "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min} "
+it 'can send a text if a phone number is provided' do 
+	expect(message).to receive(:send_text).with(:number)
+	expect(sms.send_text('')).to be true
 	
-# 	sms.send_text(customer_number)
-# 	expect(sms.send_text(phone_no)).not_to be nil
-# end
+end
 
 
 
 
 
 
-# end
+end
