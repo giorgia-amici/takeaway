b1808a08abe253a2016b1aa770d6d9c565afe033
Giorgia <giorgia1london@gmail.com>
2014-11-11 23:55:56 +0000

    starting the bill class

--- spec/bill_spec.rb
+++ spec/bill_spec.rb
@@ -2,7 +2,15 @@ require 'bill'
 
 describe Bill do 
 
+it 'will contain each dish'
 
+it 'will contain the price assigned to each dish'
+
+it 'will contain the quantity of an item'
+
+it 'will line up all the elements giving the due amount for each dish'
+
+it 'will calculate the total'
 
 
 end
